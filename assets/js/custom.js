7d9867278c69bc61c787d74a079fd54812f67a66
kuhinjskikutak <151607688+kuhinjskikutak@users.noreply.github.com>
2023-12-03 17:29:43 +0100

    asd

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,9 +1,21 @@
-let slideIndex = [1,1,1];
+let slideIndex = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1];
 /* Class the members of each slideshow group with different CSS classes */
-let slideId = ["mySlides1", "mySlides2", "mySlides3" ]
+let slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4", "mySlides5", "mySlides6", "mySlides7", "mySlides8",
+ "mySlides9", "mySlides10", "mySlides11", "mySlides12", "mySlides13", "mySlides14" ]
 showSlides(1, 0);
 showSlides(1, 1);
 showSlides(1, 2);
+showSlides(1, 3);
+showSlides(1, 4);
+showSlides(1, 5);
+showSlides(1, 6);
+showSlides(1, 7);
+showSlides(1, 8);
+showSlides(1, 9);
+showSlides(1, 10);
+showSlides(1, 11);
+showSlides(1, 12);
+showSlides(1, 13);
 
 function plusSlides(n, no) {
   showSlides(slideIndex[no] += n, no);
