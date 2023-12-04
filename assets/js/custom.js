ab0cdf8a071ca363dafd056a292c73cc1f8a48e8
kuhinjskikutak <151607688+kuhinjskikutak@users.noreply.github.com>
2023-12-04 19:04:48 +0100

    d

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,7 +1,8 @@
-let slideIndex = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1];
+let slideIndex = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1];
 /* Class the members of each slideshow group with different CSS classes */
 let slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4", "mySlides5", "mySlides6", "mySlides7", "mySlides8",
- "mySlides9", "mySlides10", "mySlides11", "mySlides12", "mySlides13", "mySlides14", "mySlides15", "mySlides16", "mySlides17" ]
+ "mySlides9", "mySlides10", "mySlides11", "mySlides12", "mySlides13", "mySlides14", "mySlides15", "mySlides16", "mySlides17",
+  "mySlides18", "mySlides19", "mySlides20", "mySlides21", "mySlides22", "mySlides23", "mySlides24", "mySlides25", "mySlides26", "mySlides27" ]
 showSlides(1, 0);
 showSlides(1, 1);
 showSlides(1, 2);
@@ -19,6 +20,16 @@ showSlides(1, 13);
 showSlides(1, 14);
 showSlides(1, 15);
 showSlides(1, 16);
+showSlides(1, 17);
+showSlides(1, 18);
+showSlides(1, 19);
+showSlides(1, 20);
+showSlides(1, 21);
+showSlides(1, 22);
+showSlides(1, 23);
+showSlides(1, 24);
+showSlides(1, 25);
+showSlides(1, 26);
 
 function plusSlides(n, no) {
   showSlides(slideIndex[no] += n, no);
