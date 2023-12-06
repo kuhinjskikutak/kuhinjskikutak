4d1e93b6e679340a62babfc208d0bfa50c05038c
kuhinjskikutak <151607688+kuhinjskikutak@users.noreply.github.com>
2023-12-06 16:51:14 +0100

    asd

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,49 +1,23 @@
-let slideIndex = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1];
-/* Class the members of each slideshow group with different CSS classes */
-let slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4", "mySlides5", "mySlides6", "mySlides7", "mySlides8",
- "mySlides9", "mySlides10", "mySlides11", "mySlides12", "mySlides13", "mySlides14", "mySlides15", "mySlides16", "mySlides17",
-  "mySlides18", "mySlides19", "mySlides20", "mySlides21", "mySlides22", "mySlides23", "mySlides24", "mySlides25", "mySlides26", "mySlides27" ]
-showSlides(1, 0);
-showSlides(1, 1);
-showSlides(1, 2);
-showSlides(1, 3);
-showSlides(1, 4);
-showSlides(1, 5);
-showSlides(1, 6);
-showSlides(1, 7);
-showSlides(1, 8);
-showSlides(1, 9);
-showSlides(1, 10);
-showSlides(1, 11);
-showSlides(1, 12);
-showSlides(1, 13);
-showSlides(1, 14);
-showSlides(1, 15);
-showSlides(1, 16);
-showSlides(1, 17);
-showSlides(1, 18);
-showSlides(1, 19);
-showSlides(1, 20);
-showSlides(1, 21);
-showSlides(1, 22);
-showSlides(1, 23);
-showSlides(1, 24);
-showSlides(1, 25);
-showSlides(1, 26);
+const slideIndex = Array.from({ length: 100}, () => 1);
+const slideId = Array.from({ length: 100 }, (_, i) => `mySlides${i + 1}`);
+
+for (let i = 0; i < slideId.length; i++) {
+  showSlides(1, i);
+}
 
 function plusSlides(n, no) {
   showSlides(slideIndex[no] += n, no);
 }
 
 function showSlides(n, no) {
-  let i;
-  let x = document.getElementsByClassName(slideId[no]);
-  if (n > x.length) {slideIndex[no] = 1}
-  if (n < 1) {slideIndex[no] = x.length}
-  for (i = 0; i < x.length; i++) {
+  const x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length || n < 1) {
+    slideIndex[no] = (n > x.length) ? 1 : x.length;
+  }
+  for (let i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
-  x[slideIndex[no]-1].style.display = "block";
+  x[slideIndex[no] - 1].style.display = "block";
 }
 
 //Get the button
--- assets/js/custom1.js
+++ assets/js/custom1.js
@@ -1,31 +1,21 @@
+const slideIndex = Array.from({ length: 100}, () => 1);
+const slideId = Array.from({ length: 100 }, (_, i) => `mySlides${i + 1}`);
 
-let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+for (let i = 0; i < slideId.length; i++) {
+  showSlides(1, i);
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
 }
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("demo");
-  let captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+function showSlides(n, no) {
+  const x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length || n < 1) {
+    slideIndex[no] = (n > x.length) ? 1 : x.length;
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+  for (let i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
+  x[slideIndex[no] - 1].style.display = "block";
 }
